8abb071c3b38ae2e8d043edbf30fbd670dd6bbcc
Ma, Jia-Jun <hoxide@gmail.com>
2025-04-02 18:46:07 +0800

    update

--- Game/Levels/BasicGroupTheory.lean
+++ Game/Levels/BasicGroupTheory.lean
@@ -3,7 +3,7 @@ import Game.Levels.BasicGroupTheory.L01_identity
 import Game.Levels.BasicGroupTheory.L02_assoc
 import Game.Levels.BasicGroupTheory.L03_inverse
 import Game.Levels.BasicGroupTheory.L04_inverse_unique
-import Game.Levels.BasicGroupTheory.L05_Zn
+import Game.Levels.BasicGroupTheory.L05_Zn_new
 import Game.Levels.BasicGroupTheory.L06_Subgroup
 import Game.Levels.BasicGroupTheory.L07_Commutator
 import Game.Levels.BasicGroupTheory.L08_ElementaryTwoGroup
