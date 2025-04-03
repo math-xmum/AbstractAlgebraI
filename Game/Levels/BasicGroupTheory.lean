041e329b5393582997c0b37d314e79d000ff31a1
Ma, Jia-Jun <hoxide@gmail.com>
2025-04-03 09:52:48 +0800

    remove llmlean

--- Game/Levels/BasicGroupTheory.lean
+++ Game/Levels/BasicGroupTheory.lean
@@ -8,6 +8,9 @@ import Game.Levels.BasicGroupTheory.L06_kZ
 import Game.Levels.BasicGroupTheory.L07_Zn
 import Game.Levels.BasicGroupTheory.L08_Commutator
 import Game.Levels.BasicGroupTheory.L09_ElementaryTwoGroup
+import Game.Levels.BasicGroupTheory.L10_Coset_mem
+import Game.Levels.BasicGroupTheory.L11_Coset_def
+import Game.Levels.BasicGroupTheory.L12_Coset_eq
 
 World "BasicGroupTheory"
 Title "Basic Group Theory"
