d6b625e64aac1803ae4b3244ff8a933cc19e4d37
Ma, Jia-Jun <hoxide@gmail.com>
2025-09-30 13:08:49 +0800

    UP

---
+++ Game/Levels/Partition.lean
@@ -0,0 +1,14 @@
+import Game.Levels.Partition.L01_PartitionDefinition
+import Game.Levels.Partition.L02_PartitionEvenOdd
+import Game.Levels.Partition.L03_PartitionFiber
+import Game.Levels.Partition.L04_PartitionModn
+
+
+World "Partition"
+
+Title "Partition"
+
+Introduction "In this world we learn the definition and properties of partitions."
+
+
+Game "Partition"
