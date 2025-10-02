e98432e958496db41e9083e29bec7394ab83d4a9
Ma, Jia-Jun <hoxide@gmail.com>
2025-10-02 12:16:22 +0800

    UP

--- Game/Lemmas/Logic.lean
+++ Game/Lemmas/Logic.lean
@@ -6,6 +6,7 @@ import Mathlib.Data.Setoid.Partition
 /--
 For any type `α` and elements `a`, `b`, and `c`, the membership of `c` in the set `{a, b}` is equivalent to `c` being equal to `a` or `b`.
 -/
+@[simp]
 lemma Set.mem_pair_iff {α :Type*} {a b c : α} :  c ∈ ({a, b} : Set α ) ↔ c = a ∨ c = b := by
   rw [Set.mem_insert_iff, Set.mem_singleton_iff]
 
--- Game/Levels/Partition/L02_PartitionEvenOdd.lean
+++ Game/Levels/Partition/L02_PartitionEvenOdd.lean
@@ -6,6 +6,15 @@ Level 2
 
 variable {α : Type*} (C : Set (Set α))
 
+Introduction "We will prove that the set of even and odd numbers is a partition of the natural numbers.
+
+More precisely, let Seven := {x : ℕ | Even x} and Sodd := {x : ℕ | Odd x}.
+We will prove that C := {Seven, Sodd} is a partition of the natural numbers.
+
+One need a lemma called `Set.mem_pair_iff` to translate the statement that c ∈ {a,b}
+if and only if c = a or c = b.
+"
+
 
 
 Statement :
@@ -13,44 +22,89 @@ Statement :
   let Sodd := {x : ℕ  | Odd x}
   let C := {Seven, Sodd}
   Setoid.IsPartition C := by
+  Hint "We start by unfolding the definition of a partition. Use `unfold Setoid.IsPartition`."
+  unfold Setoid.IsPartition
+  Hint "Now we need to prove that the collection is not empty and that each element belongs to exactly one of \{{Seven}, {Sodd}}. Use `constructor`."
   constructor
-  · rw [Set.mem_pair_iff]
+  · Hint "Use `rw [Set.mem_pair_iff]` to translate the statement that ∅ ∈  C if and only if ∅ = {Seven} or ∅ = {Sodd}."
+    rw [Set.mem_pair_iff]
+    Hint "
+    There is few ways to prove the goal. `push_neg` is one of the way to simplify the statement having negations.
+    Use `push_neg` to push the negation inside the statement."
     push_neg
+    Hint "Use `constructor` to split the goal."
     constructor
-    use 0; decide
+    Hint "To show a set is Nonempty is to provide an element of the set. Use `use 0` and reduce the goal to show that 0 ∈ {Seven}."
+    use 0
+    Hint "This is decidable by Lean and is trivial. Use `decide` or `trivial`. "
+    decide
+    Hint "One can combine two commands in one line using `;`. Try `use 1; decide`."
     use 1; decide
-  · intro a
+  · Hint "We need to prove that each element belongs to exactly one of \{{Seven}, {Sodd}}. Use `intro a`."
+    intro a
+    Hint "Use `by_cases` to split the goal into two cases: `Even a` and `¬ Even a`."
     by_cases h : Even a
+    Hint "Use `use Seven` to show that `a` belongs to `Seven`."
     use Seven
+    Hint "Use `simp` to make the goal more readable."
     simp
+    Hint "Use `constructor` to split the goal."
     constructor
-    · rw [Set.mem_pair_iff]
-      constructor
-      · left
-        rfl
-      · exact h
-    · intro S hS ha
-      rw [Set.mem_pair_iff] at hS
-      rcases hS with H | H
-      exact H
-      rw [H] at ha
-      exfalso
-      rw [<-Nat.not_odd_iff_even] at h
-      exact h ha
-    · rw [Nat.not_even_iff_odd] at h
-      use Sodd
-      simp
+    · Hint "Use `rw [Set.mem_pair_iff]` to translate the statement that `Seven` belongs to `C` if and only if `Seven` = `Seven` or `Seven` = `Sodd`."
       rw [Set.mem_pair_iff]
-      simp
+      Hint "Use `constructor` to split the goal."
       constructor
-      exact h
+      · Hint (hidden := true) "Use `left` to show that `Seven` = `Seven`."
+        left
+        Hint (hidden := true) "Use `rfl` to show that `Seven` = `Seven`."
+        rfl
+      · Hint (hidden := true) "Use `exact h` to show that `{a} ∈  {Seven}`."
+        exact h
+    · Hint "Use `intro S hS ha` to introduce the hypothesis."
       intro S hS ha
+      Hint "Use `rw [Set.mem_pair_iff]` to translate the statement that `S` belongs to `C` if and only if `S` = `Seven` or `S` = `Sodd`."
+      rw [Set.mem_pair_iff] at hS
+      Hint "Use `rcases {hS} with H | H` to split the goal into two cases: `H` and `H`."
       rcases hS with H | H
-      · rw [H] at ha
+      Hint "This is exactly {H}."
+      exact H
+      Hint "Use `rw [{H}] at {ha}` to rewrite {ha}."
+      rw [H] at ha
+      Hint "Use `exfalso` to indicate that our the goal is show that the assumptions are contradictory."
+      exfalso
+      Hint "Use `rw [<-Nat.not_odd_iff_even] at h` to rewrite {h}."
+      rw [<-Nat.not_odd_iff_even] at h
+      Hint "Use `exact h ha` to show that the goal is false. One also can use `contradiction`."
+      exact h ha
+    · Hint "The proof is similar to the previous case. "
+      Hint (hidden := true) "Use `rw [Nat.not_even_iff_odd] at h` to rewrite {h}."
+      rw [Nat.not_even_iff_odd] at h
+      Hint (hidden := true) "Use `use Sodd` to show that `a` belongs to `Sodd`."
+      use Sodd
+      Hint (hidden := true) "Use `simp` to make the goal more readable."
+      simp
+      Hint (hidden := true) "Use `rw [Set.mem_pair_iff]` to translate the statement that `S` belongs to `C` if and only if `S` = `Seven` or `S` = `Sodd`."
+      rw [Set.mem_pair_iff]
+      Hint (hidden := true) "Use `simp` to make the goal more readable."
+      simp
+      Hint (hidden := true) "Use `constructor` to split the goal."
+      constructor
+      Hint (hidden := true) "Use `exact h` to show that `{a} ∈ {Sodd}`."
+      exact h
+      Hint (hidden := true) "Use `intro S hS ha` to introduce the hypothesis."
+      intro S hS ha
+      Hint (hidden := true) "Use `rcases {hS} with H | H` to split the goal into two cases: `H` and `H`."
+      rcases hS with H | H
+      · Hint (hidden := true) "Use `rw [H] at ha` to rewrite {ha}."
+        rw [H] at ha
+        Hint (hidden := true) "Use `exfalso` to indicate that our the goal is show that the assumptions are contradictory."
         exfalso
+        Hint (hidden := true) "Use `rw [<-Nat.not_even_iff_odd] at h` to rewrite {h}."
         rw [<-Nat.not_even_iff_odd] at h
+        Hint "Use `exact h ha` to show that the goal is false. One also can use `contradiction`."
         exact h ha
-      · exact H
+      · Hint (hidden := true) "Use `exact H` to close the goal."
+        exact H
 
-OnlyTactic rfl
-NewTheorem Set.mem_pair_iff
+NewTactic exfalso trivial «have» simp trivial push_neg contradiction
+NewTheorem Set.mem_pair_iff Nat.not_odd_iff_even Nat.not_even_iff_odd
