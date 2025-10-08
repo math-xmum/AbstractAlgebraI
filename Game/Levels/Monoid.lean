2de89877b4cc8028f2ec44498f472eaf8f80395e
Ma, Jia-Jun <hoxide@gmail.com>
2025-10-09 01:28:02 +0800

    UP

---
+++ Game/Levels/Monoid.lean
@@ -0,0 +1,15 @@
+import Game.Levels.Monoid.L01_MonoidDef
+import Game.Levels.Monoid.L02_MonoidFunction
+import Game.Levels.Monoid.L03_LeftInverseEqRightInverse
+
+
+
+
+World "Monoid"
+
+Introduction "
+A monoid is a set with a binary operation that is closed under that operation is associative
+and has an identity element.
+
+In another word, a monoid is a semigroup with an identity element.
+"
