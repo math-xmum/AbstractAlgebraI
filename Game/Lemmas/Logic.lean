246ccd81ef314c6e46cd78353339a11bcf2b1036
Ma, Jia-Jun <hoxide@gmail.com>
2025-10-10 10:06:26 +0800

    UP

--- Game/Lemmas/Logic.lean
+++ Game/Lemmas/Logic.lean
@@ -11,4 +11,28 @@ lemma Set.mem_pair_iff {α :Type*} {a b c : α} :  c ∈ ({a, b} : Set α ) ↔ 
   rw [Set.mem_insert_iff, Set.mem_singleton_iff]
 
 lemma Function.fiber_of_surjective_partition {α β : Type*} {f : α → β} (h : Function.Surjective f) : Setoid.IsPartition $ Set.range (fun (y : β) ↦ f ⁻¹' {y}) := by
-   sorry
+   unfold Setoid.IsPartition
+   constructor
+   · intro he
+     obtain ⟨y,he⟩ := he
+     obtain ⟨x,hx⟩ := h y
+     have hx: x ∈ f ⁻¹' {y} := hx
+     beta_reduce at he
+     rw [he] at hx
+     contradiction
+   · intro a
+     use (f ⁻¹' {f a})
+     beta_reduce
+     constructor
+     · simp only [Set.mem_range, exists_apply_eq_apply, Set.mem_preimage, Set.mem_singleton_iff,
+       and_self]
+     · intro y
+       intro ⟨hy1,hy2⟩
+       obtain ⟨z,hz1⟩ := hy1
+       beta_reduce at hz1
+       rw  [<-hz1] at hy2
+       rw [<-hz1]
+       have hz2 : f a = z := by
+        rw [Set.mem_preimage, Set.mem_singleton_iff] at hy2
+        exact hy2
+       rw [hz2]
